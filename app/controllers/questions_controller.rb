3b7c2c9f2d05e3be24a35dfbeebf61c5fdbc9f69
Julien Bouland <bouland.julien@gmail.com>
2020-05-11 14:59:11 +0200

    apps working good need some css

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,15 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @ask = params[:ask]
+    if @ask == "I am going to work"
+      return @answer = "Great"
+    elsif @ask.include?("?")
+      return @answer = "Silly question, get dressed and go to work!"
+    else
+      return @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
